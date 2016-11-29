b581c3a63382f22485cc4f2123cfb497f6065a1b
litao <litaowork@aliyun.com>
2016-11-29 10:31:59 +0800

    增加 react-router

--- template/src/components/Index.js
+++ template/src/components/Index.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react' 
 import { Link } from 'react-router'
 
-import Index from './Index'
-
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h2>Hello React</h2>
-        <Link to="/about">点击跳转到About页面</Link>
-        {this.props.children || <Index />}
+      	<h3>Index Page</h3>
       </div>
     )
   }
--- template/src/index.js
+++ template/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './components/App'
-
+import { Router, Route, hashHistory, browserHistory } from 'react-router'
+// 路由配置
+import routes from './routes'
+// 全局样式
 import './styles/main.scss'
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('app')
+	<Router history={hashHistory} routes={routes} />,
+  	document.getElementById('app')
 )
